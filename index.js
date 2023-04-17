1fd70b1146cefd9c3ee6b402d779c1baa0584baa
Victor Muhita <muhitavictor69@gmail.com>
2023-04-17 11:37:52 +0300

    add winner is?

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //console.log("hi there");
 
-const gameChar = ["rock","paper","scissors"]
+const gameChar = ["rock","paper","scissors"];
+const winner = [];
+
 
 function getComputerChoise(){
     const random = Math.floor(Math.random()*3);
@@ -11,7 +13,7 @@ function getComputerChoise(){
 function getPlayerChoise(){
     const getPlayerChoises = prompt("To play the game choose between : rock , paper or scissors");
     if(getComputerChoise != "rock" ||getComputerChoise != "paper" ||getComputerChoise != "scissors"){
-        
+
     }
     return getPlayerChoises
 }
@@ -20,11 +22,35 @@ function playRound() {
     let ans = []
     ans.push(getComputerChoise(),getPlayerChoise());
     if(ans[0] == ans[1]){
+        winner.push("draw")
         console.log( "Its a draw")
+
     }else if(ans[0]==="rock"&& ans[1]==="paper" ||ans[0]==="paper" && ans[1]==="scissors" || ans[0] === "scissors"&& ans[1]==="rock"){
+        winner.push("computer")
         console.log( "***You won!!!***")
-    }else console.log( "You lost")
+        
+    }else {
+        winner.push("player")
+        console.log( "You lost")
+    }
 }
 
-playRound()
+function gameAll () {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log(winner)
+
+}
+gameAll()
+
+//code from  stackoverflow to check which checks which element occurs more in an array
+function mode(arr){
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length
+        - arr.filter(v => v===b).length
+    ).pop();
+}
+
+console.log(`the winner is ${mode(winner)}`);
 
