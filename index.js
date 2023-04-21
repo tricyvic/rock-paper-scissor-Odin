c9636db205d49f7938dabd5c875186abc7fc9db9
Victor Muhita <muhitavictor69@gmail.com>
2023-04-21 20:45:00 +0300

    start adding ui

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 //console.log("hi there");
-
+const btn = document.querySelectorAll('.choose');
 const gameChar = ["rock","paper","scissors"];
-const winner = [];
+let playerWins = 0;
+let computerWins = 0;
+const winMessage = "";
 
 
+//computer chooses between the gameChoises
 function getComputerChoise(){
     const random = Math.floor(Math.random()*3);
     //console.log(gameChar[random])
     return gameChar[random];
 }
 
-function getPlayerChoise(){
-    const getPlayerChoises = prompt("To play the game choose between : rock , paper or scissors");
-    if(getComputerChoise != "rock" ||getComputerChoise != "paper" ||getComputerChoise != "scissors"){
 
-    }
-    return getPlayerChoises
-}
 
-function playRound() {
-    let ans = []
-    ans.push(getComputerChoise(),getPlayerChoise());
+btn.forEach((button) =>{button.addEventListener('click',()=>{
+        let ans = []
+    ans.push(getComputerChoise(),button.value);
     if(ans[0] == ans[1]){
-        winner.push("draw")
-        console.log( "Its a draw")
-
-    }else if(ans[0]==="rock"&& ans[1]==="paper" ||ans[0]==="paper" && ans[1]==="scissors" || ans[0] === "scissors"&& ans[1]==="rock"){
-        winner.push("computer")
-        console.log( "***You won!!!***")
-        
-    }else {
-        winner.push("player")
-        console.log( "You lost")
+        winMessage = "It's a draw!"
+        computerWins += 1;
     }
-}
 
-function gameAll () {
-    for (let i = 0; i < 5; i++) {
-        playRound();
+    //display results
+    console.log(`Cruise ; ${playerWins}: computer ; ${computerWins}`);
+
+    //display winner
+
+    if(playerWins===5){
+        console.log("player Wins");
+        playerWins=0;
+        computerWins=0;
+    }else if(computerWins === 5){
+        console.log("computerWins");
+        playerWins=0;
+        computerWins=0;
     }
-    console.log(winner)
+    
+})})
 
-}
-gameAll()
 
-//code from  stackoverflow to check which checks which element occurs more in an array
-function mode(arr){
-    return arr.sort((a,b) =>
-          arr.filter(v => v===a).length
-        - arr.filter(v => v===b).length
-    ).pop();
-}
 
-console.log(`the winner is ${mode(winner)}`);
+
+
+// function gameAll () {
+//     for (let i = 0; i < 5; i++) {
+//         playRound();
+//     }
+//     console.log(winner)
+
+// }
+// gameAll()
+
+// //code from  stackoverflow to check which checks which element occurs more in an array
+// function mode(arr){
+//     return arr.sort((a,b) =>
+//           arr.filter(v => v===a).length
+//         - arr.filter(v => v===b).length
+//     ).pop();
+// }
+
+// console.log(`the winner is ${mode(winner)}`);
 
